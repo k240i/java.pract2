59b8365fc004989324eb5c83f4ac82a50b137cbc
k240i <inessa.pineapple@gmail.com>
2022-09-20 04:18:46 +0300

    Initial commit

---
+++ src/ru/mirea/program1/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.program1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Инесса", "inessa.pineapple@gmail.com", 'f');
+        System.out.println(a);
+    }
+}
